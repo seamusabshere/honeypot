ab74894ef089f45b225924718db41b7da430dfc5
Seamus Abshere <seamus@abshere.net>
2010-05-27 08:33:41 -0400

    don't assume everything is available

--- lib/honeypot/rack.rb
+++ lib/honeypot/rack.rb
@@ -8,9 +8,10 @@ module Honeypot
     end
 
     def call(env)
-      session = env['rack.session']
-      remote_ip = IPAddr.new env['action_dispatch.remote_ip'].to_s
-      session['honeypot.last_known_remote_ip'] = remote_ip.to_s if remote_ip.public?
+      if session = env['rack.session'] and raw_remote_ip = env['action_dispatch.remote_ip']
+        remote_ip = IPAddr.new raw_remote_ip.to_s 
+        session['honeypot.last_known_remote_ip'] = remote_ip.to_s if remote_ip.public?
+      end
       @app.call env
     end
   end
