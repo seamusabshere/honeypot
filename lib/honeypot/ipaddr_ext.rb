8f91bc368a7027922345e1447f61bc856b4663f0
Seamus Abshere <seamus@abshere.net>
2010-07-02 10:21:40 -0400

    clarify the extensions to IPAddr... it's about routeability

--- lib/honeypot/ipaddr_ext.rb
+++ lib/honeypot/ipaddr_ext.rb
@@ -1,18 +1,18 @@
 # http://codesnippets.joyent.com/posts/show/7546
 class IPAddr
-  PRIVATE_RANGES = [
+  UNROUTEABLE_RANGES = [
     IPAddr.new('127.0.0.1/32'),
     IPAddr.new('10.0.0.0/8'),
     IPAddr.new('172.16.0.0/12'),
     IPAddr.new('192.168.0.0/16')
   ]
   
-  def private?
+  def unrouteable?
     return false unless self.ipv4?
-    PRIVATE_RANGES.any? { |ipr| ipr.include? self }
+    UNROUTEABLE_RANGES.any? { |ipr| ipr.include? self }
   end
 
-  def public?
-    !private?
+  def routeable?
+    !unrouteable?
   end
 end
